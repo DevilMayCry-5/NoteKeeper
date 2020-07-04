eeab067fd5d9e09cd09b625a1fea49f57d726876
pho <phonum5@gmail.com>
2020-07-04 10:31:31 +0000

    minor changes

--- app/src/main/java/com/dmc/pho/notekeeper/NoteActivity.java
+++ app/src/main/java/com/dmc/pho/notekeeper/NoteActivity.java
@@ -3,13 +3,9 @@ package com.dmc.pho.notekeeper;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -20,7 +16,8 @@ import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
-    public static final String NOTE_INFO = "com.dmc.pho.notekeeper";
+    public static final String NOTE_POSITION = "com.dmc.pho.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
     private boolean mIsNewNote;
 
@@ -59,8 +56,10 @@ public class NoteActivity extends AppCompatActivity {
 
     private void readDisplayStateValues() {
         Intent intent = getIntent();
-        mNote = intent.getParcelableExtra(NOTE_INFO);
-        mIsNewNote = mNote == null;
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+        mIsNewNote = position == POSITION_NOT_SET;
+        if (!mIsNewNote)
+            mNote = DataManager.getInstance().getNotes().get(position);
 
     }
 
--- app/src/main/java/com/dmc/pho/notekeeper/NotesListActivity.java
+++ app/src/main/java/com/dmc/pho/notekeeper/NotesListActivity.java
@@ -1,11 +1,9 @@
 package com.dmc.pho.notekeeper;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -33,12 +31,6 @@ public class NotesListActivity extends AppCompatActivity {
 
                 startActivity(new Intent(NotesListActivity.this, NoteActivity.class));
 
-
-                Intent sendToMail = new Intent(Intent.ACTION_SENDTO);
-                sendToMail.setData(Uri.parse("mailto:")); // only email apps should handle this
-                sendToMail.putExtra(Intent.EXTRA_TEXT, "summary");
-                sendToMail.putExtra(Intent.EXTRA_SUBJECT, "PROJECT ZEUS");
-                sendToMail.putExtra(Intent.EXTRA_EMAIL, "phonum5@outlook.com");
             }
         });
 
@@ -59,8 +51,8 @@ public class NotesListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Intent intent = new Intent(NotesListActivity.this, NoteActivity.class);
-                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_INFO, note);
+//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_POSITION, position);
                 startActivity(intent);
             }
         });
